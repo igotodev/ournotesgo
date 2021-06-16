d83768fed5d9403a0816b05ce6b384fb8d2552d1
igotodev <gotodev@protonmail.com>
2021-06-16 23:33:04 +0300

    readme

--- main.go
+++ main.go
@@ -18,8 +18,7 @@ import (
 	"github.com/muesli/termenv"
 )
 
-//const signDB string = "root:password@tcp(localhost:3306)/yourdb" // example
-const signDB string = "root:mB5nE!28JJpn@tcp(localhost:3306)/mygodb"
+const signDB string = "root:password@tcp(localhost:3306)/yourdb" // example
 
 var myDB *sql.DB
 
